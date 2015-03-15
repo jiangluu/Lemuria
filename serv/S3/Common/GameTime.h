742e48b240b28615fcc5d724c748fc4fc73f369c
jl <jiangluu@users.noreply.github.com>
2015-03-15 08:38:50 +0000

    时钟类改用 clock_gettime()函数。当前时间改为不会向前跳跃

--- serv/S3/Common/GameTime.h
+++ serv/S3/Common/GameTime.h
@@ -17,6 +17,14 @@
 #include "types.h"
 
 
+#ifdef WIN32
+	#define GX_CLOCK_MONOTONIC CLOCK_MONOTONIC
+	#define GX_CLOCK_REALTIME CLOCK_REALTIME
+#else
+	#define GX_CLOCK_MONOTONIC CLOCK_MONOTONIC_COARSE
+	#define GX_CLOCK_REALTIME CLOCK_REALTIME_COARSE
+#endif
+
 
 typedef u32 timetype;	// 游戏时间类型暂定为32位。但是这个只能撑几十天就会走完。需要时定义 timetype为u64即可 
 
@@ -55,11 +63,9 @@ struct GameTime
 		m_frame_no = 0;
 		m_local_frame_no = 0;
 		
-		struct timeval aa;
-		gettimeofday(&aa,0);
-		m_StartTime = ((u64)aa.tv_sec)*1000L+aa.tv_usec/1000;
-		
-		m_local_usec_start_time = ((u64)aa.tv_sec)*1000000L+aa.tv_usec;
+		struct timespec ss;
+		int r2 = clock_gettime(GX_CLOCK_MONOTONIC,&ss);
+		m_local_usec_start_time = ((u64)ss.tv_sec)*1000000L + ss.tv_nsec/1000;
 		m_local_usec_time = 0;
 		
 		setTime();
@@ -75,15 +81,18 @@ struct GameTime
     
     //将当前的系统时间格式化到时间管理器里
     //框架里在每一帧调用了，逻辑代码不应该再调用 
-    void            setTime(){
-    	time( &m_LocalSetTime ) ;
-    	
-    	struct timeval aa;
-	    gettimeofday(&aa,0);
-	    u64 bb = ((u64)aa.tv_sec)*1000L+aa.tv_usec/1000;
-	    m_LocalTime = timetype(bb - m_StartTime);
-	    m_local_usec_time = ((u64)aa.tv_sec)*1000000L+aa.tv_usec - m_local_usec_start_time;
-	    if(1==m_active_mode){
+    void            setTime(){	    
+	    struct timespec ss;
+	    // 不会向前跳跃的时间 
+	    int r2 = clock_gettime(GX_CLOCK_MONOTONIC,&ss);
+	    m_local_usec_time = ((u64)ss.tv_sec)*1000000L + ss.tv_nsec/1000 - m_local_usec_start_time;
+	    m_LocalTime = timetype(m_local_usec_time / 1000);
+	    
+	    // wall-clock
+	    r2 = clock_gettime(GX_CLOCK_REALTIME,&ss);
+	    m_LocalSetTime = ss.tv_sec;
+	    
+	    if(0!=m_active_mode){
 	    	m_CurrentTime = m_LocalTime;
 	    	m_SetTime = m_LocalSetTime;
 	    }
@@ -101,7 +110,7 @@ struct GameTime
     }
 
     //取得服务器端程序启动时的时间计数值
-    u64        		getStartTime(){ return (timetype)m_StartTime ; }
+    u64        		getStartTime(){ return (timetype)m_local_usec_start_time ; }
 
     
 
@@ -113,7 +122,6 @@ struct GameTime
     void			incLocalFrame(){ ++m_local_frame_no; }
 
 private:
-	u64    		 m_StartTime ;
 	int			m_active_mode;
 	timetype     m_LocalTime ;
     timetype     m_CurrentTime ;
