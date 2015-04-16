10df15530f1c020cd814e4fc2494594d512ac634
jl <jl@0967b8b8-34a7-417b-87fc-a26fa4f5a6d6>
2015-04-16 10:23:32 +0000

    service代码初始化（从router拷贝而来）

---
+++ bitme/trunk/serv/S3/service/message_dispatch.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include "service.h"
+#include "AStream.h"
+#include "GXCfunction.h"
+#include "LuaInterface.h"
+
+
+
+extern LuaInterface *g_luavm;
+
+AStream *ws = NULL;
+
+
+int message_dispatch(GXContext *gx,Link* src_link,InternalHeader *hh,int body_len,char *body)
+{
+	gx_set_context(gx);
+	
+	int msg_id = hh->message_id_;
+	if(msg_id>=8000 && msg_id<=8100){
+		int r = g_luavm->callGlobalFunc<int>("OnInternalMessage");
+	}
+	else{
+		int r = g_luavm->callGlobalFunc<int>("OnCustomMessage");
+	}
+	
+	return 0;
+}
+
+// 物理连接断掉时的回调 
+void on_client_cut(GXContext *gx,Link *ll,int reason,int gxcontext_type)
+{
+	gx_set_context(gx);
+}
+
+void frame_time_driven(timetype now)
+{
+}
+
