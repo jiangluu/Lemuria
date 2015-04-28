caa1f1d105c897815e272894a671aed6f298bb13
jl <jiangluu@users.noreply.github.com>
2015-04-28 05:14:39 +0000

    完善cfunction

--- serv/S3/service/cfunction.cpp
+++ serv/S3/service/cfunction.cpp
@@ -17,6 +17,7 @@ static char s_buf1[MAX_A_STR_LEN];
 
 #define C_ENV_SHARED_PTR_LEN 512
 
+
 void** __make_sure_c_env_get_shared_ptr_exists()
 {
 	static void** aa = NULL;
@@ -48,6 +49,102 @@ bool c_env_set_shared_ptr(int index,void *new_value)
 	p[index] = new_value;
 	return true;
 }
+
+
+bool cur_stream_is_end()
+{
+	return gx_cur_stream_is_end();
+}
+
+s16 cur_stream_get_int8()
+{
+	return gx_cur_stream_get_int8();
+}
+
+s16 cur_stream_get_int16()
+{
+	return gx_cur_stream_get_int16();
+}
+
+s32 cur_stream_get_int32()
+{
+	return gx_cur_stream_get_int32();
+}
+
+s64 cur_stream_get_int64()
+{
+	return gx_cur_stream_get_int64();
+}
+
+f32 cur_stream_get_float32()
+{
+	return gx_cur_stream_get_float32();
+}
+
+f64 cur_stream_get_float64()
+{
+	return gx_cur_stream_get_float64();
+}
+
+struct Slice cur_stream_get_slice()
+{
+	return gx_cur_stream_get_slice();
+}
+
+s16 cur_stream_peek_int16()
+{
+	return gx_cur_stream_peek_int16();
+}
+
+bool cur_stream_push_int16(s16 v)
+{
+	return gx_cur_stream_push_int16(v);
+}
+
+bool cur_stream_push_int32(s32 v)
+{
+	return gx_cur_stream_push_int32(v);
+}
+
+bool cur_stream_push_int64(s64 v)
+{
+	return gx_cur_stream_push_int64(v);
+}
+
+bool cur_stream_push_float32(f32 v)
+{
+	return gx_cur_stream_push_float32(v);
+}
+
+bool cur_stream_push_slice(struct Slice s)
+{
+	return gx_cur_stream_push_slice(s);
+}
+
+bool cur_stream_push_string(const char* v,int len)
+{	
+	return gx_cur_stream_push_slice2(v,len);
+}
+
+
+// 同步原路返回。messageid 是req的+1，内容是push到 stream里的内容。 
+void cur_stream_write_back()
+{
+	gx_cur_writestream_syncback();
+}
+
+void cur_stream_write_back2(int message_id)
+{	
+	gx_cur_writestream_syncback2(message_id);
+}
+
+void cur_stream_broadcast(int message_id)
+{
+	// @TODO
+}
+
+
+
 
 
 s32 cur_actor_id()
