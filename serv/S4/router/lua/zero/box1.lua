b196213902ec3c0cdf6f8fb40fbb098d8c93553e
jl <jiangluu@users.noreply.github.com>
2015-07-24 07:22:52 +0000

    C struct 的对齐很重要

--- serv/S4/router/lua/zero/box1.lua
+++ serv/S4/router/lua/zero/box1.lua
@@ -11,18 +11,55 @@ local ls = require('luastate')
 ffi.cdef[[
 typedef struct TransData{
 	// @TODO
+	uint32_t padding;
+	uint16_t padding2;
 	uint16_t is_active;
-	uint16_t padding;
 	uint32_t trans_id;
-	uint32_t actor_id;
 	uint32_t serial_no;
+	lua_State *co;
 } TransData;
 
 typedef struct Box{
+	uint32_t padding;
 	uint32_t box_id;
 	uint32_t actor_per_box;
-	struct lua_State *L;
+	uint32_t trans_per_box;
+	lua_State *L;
 	TransData *transdata;
+	uint32_t next_offset_transdata;
+	uint32_t next_serial_no;
 } Box;
 ]]
+
+
+function o.new_transdata(boxc)
+	for i=1, boxc.trans_per_box do
+		local aa = boxc.transdata[boxc.next_offset_transdata]
+		boxc.next_offset_transdata = (boxc.next_offset_transdata+1) % boxc.trans_per_box
+		
+		if 0==aa.is_active then
+			o.reset_transdata(aa)
+			aa.is_active = 1
+			aa.serial_no = boxc.next_serial_no
+			
+			boxc.next_serial_no = boxc.next_serial_no+1
+			
+			return aa
+		end
+	end
+	
+	return nil
+end
+
+function o.release_transdata(boxc,td)
+	assert(td>=boxc.transdata and td<boxc.transdata+boxc.trans_per_box)
+	td.is_active = 0
+end
+
+function o.reset_transdata(td)
+	local bak = td.trans_id
+	
+	ffi.fill(td,ffi.sizeof(td))
+	td.trans_id = bak
+end
 
