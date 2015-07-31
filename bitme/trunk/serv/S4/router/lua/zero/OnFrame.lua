0102f6307e9c641bef18f1f9ee54deb4a7194ad0
jl <jl@0967b8b8-34a7-417b-87fc-a26fa4f5a6d6>
2015-07-31 04:12:52 +0000

    补上GC

--- bitme/trunk/serv/S4/router/lua/zero/OnFrame.lua
+++ bitme/trunk/serv/S4/router/lua/zero/OnFrame.lua
@@ -12,5 +12,11 @@ function OnFrame()
 	-- GC self
 	if 0==(counter1 % 100) then
 		collectgarbage('step',10)
+		
+		for i=1,boxraid.box_num+1 do
+			local box = boxraid.getboxc(i)
+			ls.gc(box.L, ls.C.LUA_GCSTEP ,10)
+		end
+		
 	end
 end
