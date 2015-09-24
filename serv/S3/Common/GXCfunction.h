3c2367296ba5630c1103334c70b99bf3dc9484c2
jl <jiangluu@users.noreply.github.com>
2015-09-24 09:48:47 +0000

    支持flag（主要是为了广播）

--- serv/S3/Common/GXCfunction.h
+++ serv/S3/Common/GXCfunction.h
@@ -83,7 +83,7 @@ CF_EXPORT int gx_make_portal_sync(const char* ID,const char* port);
 
 CF_EXPORT int gx_bind_portal_id(int index,const char* id);
 
-CF_EXPORT int gx_cur_writestream_route_to(const char* destID,int message_id);
+CF_EXPORT int gx_cur_writestream_route_to(const char* destID,int message_id,int flag);
 
 }
 
