f86be6ef0f6214a746c1919489f54505f4cc1f03
jiangluu <foo@gmail.com>
2016-11-16 16:00:10 +0800

    remove polarssl

--- serv/S4/Common/GXCfunction.cpp
+++ serv/S4/Common/GXCfunction.cpp
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string>
-#include "polarssl/md5.h"
 #include "link.h"
 #include "GXContext.h"
 #include "GXCfunction.h"
--- serv/S4/router/cfunction.cpp
+++ serv/S4/router/cfunction.cpp
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string>
-#include "polarssl/md5.h"
 #include "router.h"
 #include "LuaInterface.h"
 #include "cfunction.h"
