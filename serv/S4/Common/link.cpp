3dfc688345b08dc00a1d75351b9f5743c5beba96
jiangluu <foo@gmail.com>
2016-11-25 15:17:52 +0800

    add Link::unregister_event

--- serv/S4/Common/link.cpp
+++ serv/S4/Common/link.cpp
@@ -9,6 +9,18 @@ int Link::register_read_event(GXContext *nc){
 	int r = epoll_ctl(nc->epoll_fd_, EPOLL_CTL_ADD, sock_, &ev_);
 	return r;
 }
+
+int Link::register_readwrite_event(GXContext *nc){
+	ev_.events = EPOLLIN | EPOLLOUT;
+	ev_.data.ptr = this;
+	int r = epoll_ctl(nc->epoll_fd_, EPOLL_CTL_ADD, sock_, &ev_);
+	return r;
+}
+
+int Link::unregister_event(GXContext *nc){
+	int r = epoll_ctl(nc->epoll_fd_, EPOLL_CTL_DEL, sock_ , &ev_);
+	return r;
+}
 #endif
 
 
--- serv/S4/Common/link.h
+++ serv/S4/Common/link.h
@@ -154,6 +154,8 @@ struct Link{
 	#else
 	// EPOLL，注册读事件（永久的，直到显式反注册） 
 	int register_read_event(GXContext *nc);
+	int register_readwrite_event(GXContext *nc);
+	int unregister_event(GXContext *nc);
 	#endif
 	
 	void allocate_mem(int read_buf_len,int write_fifo_len){
